20ad2e709a6cf012688fe6542d6ef6d52c55db82
Vinayagamourty <kvmourty@yahoo.com>
2019-10-28 00:27:22 +0530

    Added JavaScript.js file and Index.html file

---
+++ SourceCodeDirectory1/JavaScript.js
@@ -0,0 +1,5 @@
+const name = 'Gatsby ' ;
+const numberofyears = ' sortie en 2013 ' ;
+const creator = 'Réalisé par Baz Luhrmann' ;
+'Gatsby' + '2013' + 'Baz luhrmann'
+alert(name + creator + numberofyears);
